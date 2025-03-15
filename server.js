f528e4b696f79df7541db72fa794ee1f34bc2c59
Shivam Jangid <136138876+shivamj0303@users.noreply.github.com>
2025-03-15 14:15:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
